e967f0139de0fc78475d236db6a48af25f1d4cf1
d.yacenko <d.yacenko@gmail.com>
2015-11-24 16:48:02 +0300

    reformat layout to scroll

--- app/src/main/java/ru/itx/conduit/ui/Fragments/AccountFragment.java
+++ app/src/main/java/ru/itx/conduit/ui/Fragments/AccountFragment.java
@@ -105,8 +105,10 @@ public class AccountFragment extends FragmentTools {
 	private void drawTable(Group group) {
 		List<AccountModel> list = DataHelper.getDH()
 				.selectAll_Account(group);
-		TableLayout table = (TableLayout) view.findViewById(R.id.account_table);
-		table.removeAllViews();
+		TableLayout table_names = (TableLayout) view.findViewById(id.account_table_names);
+		TableLayout table_values = (TableLayout) view.findViewById(id.account_table_values);
+		table_names.removeAllViews();
+		table_values.removeAllViews();
 		if (list == null)
 			return;
 		// выбираем всех учащихся
@@ -117,35 +119,38 @@ public class AccountFragment extends FragmentTools {
 						&& !r.getStudent().isHidden())
 					students.add(r.getStudent());
 		//
-		Display display = getActivity().getWindowManager().getDefaultDisplay();
-		int width = display.getWidth();
-		int maxNum = (width - LEFT_COLUMN_WIDTH_PX) / COLUMN_WIDTH_PX;
-		int start = list.size() > maxNum ? list.size() - maxNum : 0;
+//		Display display = getActivity().getWindowManager().getDefaultDisplay();
+//		int width = display.getWidth();
+//		int maxNum = (width - LEFT_COLUMN_WIDTH_PX) / COLUMN_WIDTH_PX;
+//		int start = list.size() > maxNum ? list.size() - maxNum : 0;
 		// строим таблицу
-		TableRow.LayoutParams llp = new TableRow.LayoutParams(COLUMN_WIDTH_PX,
-				LayoutParams.WRAP_CONTENT, 1);
-		TableRow.LayoutParams llh = new TableRow.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+		TableRow.LayoutParams llp = new TableRow.LayoutParams(LEFT_COLUMN_WIDTH_PX,LayoutParams.WRAP_CONTENT,0.5f);
+		TableRow.LayoutParams llh = new TableRow.LayoutParams(0,LayoutParams.WRAP_CONTENT,1);
 		// table.setStretchAllColumns(false);
-		table.setShrinkAllColumns(false);
+		table_names.setShrinkAllColumns(false);
+		table_values.setShrinkAllColumns(false);
 		// фомируем шапку таблицы: поле (0,0)
-		TableRow header = new TableRow(getActivity());
-		TextView cellLeftUp = new TextView(getActivity());
-		cellLeftUp.setLayoutParams(llh);
-		cellLeftUp.setLines(3);
-		cellLeftUp.setMaxLines(3);
-		header.addView(cellLeftUp);
-		header.addView(getVBorder());
-		for (int i = start; i < list.size(); i++) {
+		TableRow header_names = new TableRow(getActivity());
+		TextView cellLeftUp_names = new TextView(getActivity());
+		cellLeftUp_names.setLines(3);
+		cellLeftUp_names.setMaxLines(3);
+		cellLeftUp_names.setLayoutParams(llp);
+		header_names.addView(cellLeftUp_names);
+		header_names.addView(getVBorder());
+		table_names.addView(header_names);
+
+		TableRow header_values = new TableRow(getActivity());
+		for (int i = 0; i < list.size(); i++) {
 			TextView cellHeader = new TextView(getActivity());
 			cellHeader.setText(saySimple(new Date(list.get(i).date)) + "\n"
 					+ list.get(i).subject);
 			cellHeader.setTypeface(Typeface.SERIF, Typeface.BOLD);
 			cellHeader.setBackgroundColor(getResources().getColor(
 					R.color.lightgray));
-			cellHeader.setLines(2);
-			cellHeader.setMaxLines(2);
-			cellHeader.setLayoutParams(llp);
+			cellHeader.setLines(3);
+			cellHeader.setMaxLines(3);
+//			cellHeader.setLayoutParams(llh);
 			cellHeader.setRotation(-90);
 			cellHeader.setClickable(true);
 			cellHeader.setTag(list.get(i));
@@ -157,17 +162,18 @@ public class AccountFragment extends FragmentTools {
 					go2Fragment(((MainActivity)getActivity()).editAccountRecordFragment);
 				}
 			});
-			header.addView(cellHeader);
-			header.addView(getVBorder());
+			header_values.addView(cellHeader);
+			header_values.addView(getVBorder());
 		}
-		table.addView(header);
-		// строим левый хидер
+		table_values.addView(header_values);
+
+			// строим левый хидер
 		int j=0;
 		for (Student s : students) {
 			j++;
-			TableRow row = new TableRow(getActivity());
+			TableRow row_names = new TableRow(getActivity());
 			TextView cellHeader = new TextView(getActivity());
-			cellHeader.setText(s.getName() + " " + (s.getLastName()==null?"":s.getLastName()) + " "
+			cellHeader.setText(s.getName() + " " + (s.getLastName() == null ? "" : s.getLastName()) + " "
 					+ s.getSurName());
 			cellHeader.setTypeface(Typeface.SERIF, Typeface.BOLD);
 			if(j%2==0)
@@ -176,7 +182,7 @@ public class AccountFragment extends FragmentTools {
 			else
 				cellHeader.setBackgroundColor(getResources().getColor(
 						R.color.gray));
-			cellHeader.setLayoutParams(llh);
+			cellHeader.setLayoutParams(llp);
 			cellHeader.setLines(1);
 			cellHeader.setMaxLines(1);
 			cellHeader.setClickable(true);
@@ -190,10 +196,11 @@ public class AccountFragment extends FragmentTools {
 					go2Fragment(((MainActivity) getActivity()).editStudentFragment);
 				}
 			});
-			row.addView(cellHeader);
-			row.addView(getVBorder());
+			row_names.addView(cellHeader);
+			row_names.addView(getVBorder());
 
-			for (int i = start; i < list.size(); i++) {
+			TableRow row_values = new TableRow(getActivity());
+			for (int i = 0; i < list.size(); i++) {
 				String rez = "";
 				for (RowAccountRecordModel r : list.get(i).list) {
 					if (s.equals(r.getStudent())) {
@@ -202,17 +209,18 @@ public class AccountFragment extends FragmentTools {
 				}
 				TextView cell = new TextView(getActivity());
 				cell.setText(rez);
-				cell.setLayoutParams(llp);
+				cell.setLayoutParams(llh);
 				if(j%2==0)
 					cell.setBackgroundColor(getResources().getColor(R.color.white));
 				else
 					cell.setBackgroundColor(getResources().getColor(R.color.lightpink));
 				cell.setGravity(Gravity.CENTER);
-				row.addView(cell);
-				row.addView(getVBorder());
+				row_values.addView(cell);
+				row_values.addView(getVBorder());
 			}
 
-			table.addView(row);
+			table_names.addView(row_names);
+			table_values.addView(row_values);
 		}
 		List<TableRow> listRow = new ArrayList<TableRow>();
 	}
